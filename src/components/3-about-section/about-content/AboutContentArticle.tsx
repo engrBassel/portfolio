41c79eeec02d3b4feea0f2639f9e715ef93c1b50
engrBassel <engrbassel@outlook.com>
2024-04-29 23:15:34 +0300

    Editing resume and about section

--- src/components/3-about-section/about-content/AboutContentArticle.tsx
+++ src/components/3-about-section/about-content/AboutContentArticle.tsx
@@ -1,8 +1,8 @@
 function AboutContentArticle() {
   return (
     <article>
-      I'm a Junior React Frontend Developer Position, I've good knowledge of
-      HTML, CSS, JavaScript and React to create responsive and user-friendly web
+      I'm an entry level frontend developer, I've good knowledge of HTML, CSS,
+      JavaScript and React to create responsive and user-friendly web
       applications. I've no professional experience but I'm an ambitious person
       seeking to gain experience and learn. I've completed several projects and
       courses in web development, and I'm eager to learn from the experience of
