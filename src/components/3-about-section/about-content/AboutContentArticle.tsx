e56d77b9b3cf6a78ba74177af1d7591ff3e27c15
engrBassel <engrbassel@outlook.com>
2024-05-12 18:55:26 +0300

    Updating resume, summary and projects

--- src/components/3-about-section/about-content/AboutContentArticle.tsx
+++ src/components/3-about-section/about-content/AboutContentArticle.tsx
@@ -1,12 +1,12 @@
 function AboutContentArticle() {
   return (
     <article>
-      I'm an entry level frontend developer, I've good knowledge of HTML, CSS,
-      JavaScript and React to create responsive and user-friendly web
-      applications. I've no professional experience but I'm an ambitious person
-      seeking to gain experience and learn. I've completed several projects and
-      courses in web development, and I'm eager to learn from the experience of
-      working at a big company.
+      Computer Engineering graduate with a passion for crafting user-friendly
+      web applications. Skilled in modern front-end development (ReactJS,
+      TypeScript, Vite) and building responsive UIs (HTML5, CSS3,
+      Bootstrap/Tailwind). Experienced with state management (Redux Toolkit,
+      Context API) and data fetching (Axios). Eager to contribute to a
+      collaborative team environment and learn from experienced professionals.
     </article>
   );
 }
