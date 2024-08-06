314de0fa88926db390e8ccbf558f7f6b90ee0b82
BasselMostafa <engrbassel@outlook.com>
2024-08-06 15:53:01 +0300

    Removing unnecessary projects and images

--- src/components/5-projects-section/projects/projectsImgs.ts
+++ src/components/5-projects-section/projects/projectsImgs.ts
@@ -1,35 +1,17 @@
 import prayerTimesImgSrc from "../../../assets/images/prayer-times.png";
 import thisPortfolioImgSrc from "../../../assets/images/this-portfolio.png";
-import shoppingCartImgSrc from "../../../assets/images/shopping-cart.png";
 import hangmanGameImgSrc from "../../../assets/images/hangman-game.png";
 import quizAppImgSrc from "../../../assets/images/quiz-app.png";
 import memoryGameImgSrc from "../../../assets/images/memory-game.png";
 import typingTestImgSrc from "../../../assets/images/typing-test.png";
-import imageSliderImgSrc from "../../../assets/images/image-slider.png";
-import serialGeneratorImgSrc from "../../../assets/images/serial-generator.png";
-import reposFetchImgSrc from "../../../assets/images/github-repos-fetching.png";
-import countdownTimerImgSrc from "../../../assets/images/countdown-timer.png";
-import template4ImgSrc from "../../../assets/images/template4.png";
-import template3ImgSrc from "../../../assets/images/template3.png";
-import template2ImgSrc from "../../../assets/images/template2.png";
-import template1ImgSrc from "../../../assets/images/template1.png";
 
 const imgSrcs = [
   prayerTimesImgSrc,
   thisPortfolioImgSrc,
-  shoppingCartImgSrc,
   hangmanGameImgSrc,
   quizAppImgSrc,
   memoryGameImgSrc,
   typingTestImgSrc,
-  imageSliderImgSrc,
-  serialGeneratorImgSrc,
-  reposFetchImgSrc,
-  countdownTimerImgSrc,
-  template4ImgSrc,
-  template3ImgSrc,
-  template2ImgSrc,
-  template1ImgSrc,
 ];
 
 export default imgSrcs;
