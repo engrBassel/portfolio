50cdf673041cfa68925ed2b5a56ef8107496436f
BasselMostafa <engrbassel@outlook.com>
2024-09-13 00:00:24 +0300

    Update Projects

--- src/components/5-projects-section/projects/projectsImgs.ts
+++ src/components/5-projects-section/projects/projectsImgs.ts
@@ -1,5 +1,7 @@
+import businessImgSrc from "../../../assets/images/business.png";
+import ecommerceImgSrc from "../../../assets/images/ecommerce.png";
 import thisPortfolioImgSrc from "../../../assets/images/this-portfolio.png";
 
-const imgSrcs = [thisPortfolioImgSrc];
+const imgSrcs = [businessImgSrc, ecommerceImgSrc, thisPortfolioImgSrc];
 
 export default imgSrcs;
